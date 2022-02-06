a3d38d85b4278252382bbe88faa60b31efa55c02
SadElephant <angelforevor@mail.ru>
2022-02-06 20:06:35 +0300

    commit 1

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,4 +2,32 @@
 @section('title-block')Контакты@endsection
 @section('content')
     <h1>Контакты</h1>
+    <p>Разнообразный и богатый опыт реализация намеченных плановых заданий способствует подготовки и реализации систем
+        массового участия. С другой стороны реализация намеченных плановых заданий влечет за собой процесс внедрения и
+        модернизации новых предложений. Задача организации, в особенности же дальнейшее развитие различных форм
+        деятельности в значительной степени обуславливает создание соответствующий условий активизации. Таким образом
+        укрепление и развитие структуры способствует подготовки и реализации соответствующий условий активизации.</p>
+    <form action="{{ route('contact-form') }}" method="post">
+        @csrf
+        <div class="form-group">
+            <label for="name">Введите имя</label>
+            <input typeof="text" name="name" placeholder="Введите имя" id="name" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="email">Email</label>
+        <input typeof="text" name="email" placeholder="Email" id="email" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="subject">Тема сообщения</label>
+            <input typeof="text" name="subject" placeholder="Тема сообщения" id="subject" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="message">Сообщение</label>
+            <textarea name="message" id="message" class="form-control" placeholder="Введите сообщение"></textarea>
+        </div>
+        <button type="submit" class="btn btn-success">Отправить</button>
+
+
+
+    </form>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,15 +3,25 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+          integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/app.css">
 
     <title>Блог @yield('title-block')</title>
 </head>
 <body class="antialiased">
 @include('inc.header')
-@yield('content')
-@include('inc.aside')
+@if(Request::is('/'))
+    @include('inc.hero')
+@endif
+
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-8">@yield('content')</div>
+        <div class="col-4">@include('inc.aside')</div>
+    </div>
+</div>
 @include('inc.footer')
 </body>
 </html>
